4c33546e6ee885f72b225cedd8721f73b89ddea6
BT <btdevcloud@gmail.com>
2020-12-06 01:38:10 +0100

    copy to clipboard

--- src/app/views/appointment/appointment.component.ts
+++ src/app/views/appointment/appointment.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-appointment',
-  templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.scss']
+  selector: "app-appointment",
+  templateUrl: "./appointment.component.html",
+  styleUrls: ["./appointment.component.scss"],
 })
 export class AppointmentComponent implements OnInit {
+  public contactInfo = {
+    mail: "atb-first@gmail.com",
+    phone: "+49 176 423544633",
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public onCopyToClipboard(value: any): void {
+    (window.navigator as any).clipboard
+      .writeText(value)
+      .then()
+      .catch((e) => console.error(e));
   }
-
 }
--- src/app/views/landing-page/components/info-company/info-company.component.ts
+++ src/app/views/landing-page/components/info-company/info-company.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { AppointmentComponent } from "src/app/views/appointment/appointment.component";
 
 @Component({
-  selector: 'app-info-company',
-  templateUrl: './info-company.component.html',
-  styleUrls: ['./info-company.component.scss']
+  selector: "app-info-company",
+  templateUrl: "./info-company.component.html",
+  styleUrls: ["./info-company.component.scss"],
 })
 export class InfoCompanyComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onMakeAppointment() {
+    this.dialog.open(AppointmentComponent);
   }
-
 }
